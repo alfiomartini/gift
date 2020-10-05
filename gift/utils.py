f7a32ce09173a2fed5e75388acc9b3838baef78b
Alfio Martini <alfio.martini@gmail.com>
2020-10-05 10:13:58 -0300

    Date computation in views now with a dedicated function

--- gift/utils.py
+++ gift/utils.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from datetime import datetime
 
 GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 headers = {'Authorization': f"token {GITHUB_TOKEN}",
@@ -32,5 +33,22 @@ def getUserRepos(username):
     return resp
 
 
-def format_date():
-    pass
+# see: https://stackabuse.com/converting-strings-to-datetime-in-python/
+def formatRep(rep_object):
+    for rep in rep_object:
+        created_str = rep['created_at'].replace('T', ' ')
+        created_str = created_str.replace('Z', '')
+        created_at = datetime.strptime(
+            created_str, "%Y-%m-%d %H:%M:%S")
+        rep['created_at'] = created_at
+
+        updated_str = rep['updated_at'].replace('T', ' ')
+        updated_str = updated_str.replace('Z', '')
+        updated_at = datetime.strptime(
+            updated_str, "%Y-%m-%d %H:%M:%S")
+        rep['updated_at'] = updated_at
+
+        cropped_name = rep['name'][0:15]
+        new_name = cropped_name + ' '
+        rep['name'] = new_name
+    return rep_object
--- gift/views.py
+++ gift/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
-from .utils import getGitUser, getUserRepos
-from datetime import datetime
+from .utils import getGitUser, getUserRepos, formatRep
 
 # Create your views here.
 
@@ -20,24 +19,7 @@ def user_render(request, username):
                       {'message': user_resp['error']})
     else:
         repos_resp = getUserRepos(username)
-        #  maps date iso strings into datetime objects
-        # see: https://stackabuse.com/converting-strings-to-datetime-in-python/
-        for rep in repos_resp:
-            created_str = rep['created_at'].replace('T', ' ')
-            created_str = created_str.replace('Z', '')
-            created_at = datetime.strptime(
-                created_str, "%Y-%m-%d %H:%M:%S")
-            rep['created_at'] = created_at
-
-            updated_str = rep['updated_at'].replace('T', ' ')
-            updated_str = updated_str.replace('Z', '')
-            updated_at = datetime.strptime(
-                updated_str, "%Y-%m-%d %H:%M:%S")
-            rep['updated_at'] = updated_at
-
-            cropped_name = rep['name'][0:15]
-            new_name = cropped_name + ' '
-            rep['name'] = new_name
+        repos_resp = formatRep(repos_resp)
         repos_resp.sort(key=lambda x: x['created_at'], reverse=True)
         context = {'user': user_resp, 'repos': repos_resp}
         return render(request, 'gift/github.html', context=context)
@@ -59,21 +41,6 @@ def user_get(request, username):
         return JsonResponse({'error': user_resp['error']})
     else:
         repos_resp = getUserRepos(username)
-        for rep in repos_resp:
-            created_str = rep['created_at'].replace('T', ' ')
-            created_str = created_str.replace('Z', '')
-            created_at = datetime.strptime(
-                created_str, "%Y-%m-%d %H:%M:%S")
-            rep['created_at'] = created_at
-
-            updated_str = rep['updated_at'].replace('T', ' ')
-            updated_str = updated_str.replace('Z', '')
-            updated_at = datetime.strptime(
-                updated_str, "%Y-%m-%d %H:%M:%S")
-            rep['updated_at'] = updated_at
-
-            cropped_name = rep['name'][0:15]
-            new_name = cropped_name + ' '
-            rep['name'] = new_name
+        repos_resp = formatRep(repos_resp)
         repos_resp.sort(key=lambda x: x['created_at'], reverse=True)
         return JsonResponse({'user': user_resp, 'repos': repos_resp})
