e5695a4e61934207f98b5969b52ba36cf9ce5cf5
Alfio Martini <alfio.martini@gmail.com>
2020-10-15 21:32:19 -0300

    Updated env variables

--- github_finder/settings.py
+++ github_finder/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('GIFT_SECRET_KEY')
 
