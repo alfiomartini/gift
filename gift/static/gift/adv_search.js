cc4df41a1252d8e04d2d4a9141ac859f634b1f13
Alfio Martini <alfio.martini@gmail.com>
2020-11-02 11:46:10 -0300

    Added adv_search form validation (HTML only)

--- gift/static/gift/adv_search.js
+++ gift/static/gift/adv_search.js
@@ -7,22 +7,42 @@ function adv_search(){
     const btn_apply = document.getElementById('btn-apply');
     const btn_reset = document.getElementById('btn-reset');
     const adv_heading = document.querySelector('.header');
-    // console.log(adv_heading);
+    
    
     btn_hide.addEventListener('click', displayAdv);
     btn_apply.addEventListener('click', processForm);
     btn_reset.addEventListener('click', doReset);
+
+    const created = document.getElementById('created');
+    created.max = new Date().toISOString().split("T")[0];
+    updated = document.getElementById('updated');
+    updated.max = new Date().toISOString().split("T")[0];
 
     function doReset(event){
       event.preventDefault();
       window.location.assign('/reset_adv');
     }
 
+  
     function processForm(event){
-      event.preventDefault();
+      // event.preventDefault();
+      
+      const style = window.getComputedStyle(adv_heading);
+      const old_color = style.getPropertyValue('color');
       adv_heading.style.color = 'lightgreen';
-      // adv_heading.style.transition = 'all 1s';
-      adv_form.submit();
+      setTimeout(() => {adv_heading.style.color = old_color;}, 600);
+      
+      // if (created.value < created.min || created.value > created.max){
+      //   const modal = new Modal('');
+      //   modal.show(`Range of date values: ${created.min} - ${created.max}`);
+      // }
+      // else if (updated.value < updated.min || updated.value > updated.max){
+      //   const modal = new Modal('');
+      //   modal.show(`Range of date values: ${created.min} - ${created.max}`);
+      // }
+      // else{
+      //   adv_form.submit();
+      // }
     }
 
     function displayAdv(event){
